4e43ecd93c13b2b1f2712a5eebbdb93dfa6e3b46
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-11-15 17:56:52 +0000

    calculated income/expense/total & passed to components as props

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
      <Header />
       <div class="container">
         <Balance :total="total" />
-        <IncomeExpenses />
+        <IncomeExpenses :income="income" :expenses="expenses" />
         <TransactionList :transactions="transactions" />
         <AddTransaction />
       </div>
@@ -24,11 +24,29 @@ import { ref, computed } from 'vue';
         {id: 4, text: 'Camera', amount:150.00},
       ]);
   
-
+        // Get Total
       const total = computed(() => {
         return transactions.value.reduce((acc, transaction) => {
             return acc + transaction.amount
         }, 0)
+      })
+
+      // Get Income 
+      const income = computed(() => {
+        return transactions.value
+        .filter((transaction) => transaction.amount > 0)
+        .reduce((acc, transaction) => {
+            return acc + transaction.amount
+        }, 0).toFixed(2)
+      })
+
+// Get Expenses
+const expenses = computed(() => {
+        return transactions.value
+        .filter((transaction) => transaction.amount < 0)
+        .reduce((acc, transaction) => {
+            return acc + transaction.amount
+        }, 0).toFixed(2)
       })
   </script>
   
